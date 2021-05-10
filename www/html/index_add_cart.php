32a591c601be80cf38a74161aebdb42d443dcf79
JT <mi22mrak@gmail.com>
2021-05-11 08:37:53 +0900

    CSRF修正完了

--- www/html/index_add_cart.php
+++ www/html/index_add_cart.php
@@ -13,10 +13,9 @@ if (is_logined() === false) {
 
 $db = get_db_connect();
 $user = get_login_user($db);
-
+$token = get_post('token');
 $item_id = get_post('item_id');
 if (is_valid_csrf_token($token)) {
-  $token = get_post('token');
   if (add_cart($db, $user['user_id'], $item_id)) {
     set_message('カートに商品を追加しました。');
   } else {
