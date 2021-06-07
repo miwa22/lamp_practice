aaeeda0a5109d7eb3319821fb23f36f754f2ecee
JT <mi22mrak@gmail.com>
2021-06-07 12:49:14 +0900

    修正

--- www/html/index.php
+++ www/html/index.php
@@ -19,9 +19,7 @@ $db = get_db_connect();
 $user = get_login_user($db);
 // sql SELECT FROM itemsテーブル情報を取得
 $items = get_open_items($db);
-//$items = change_htmlsp_array($data);
 
 $ranking = get_open_ranking($db);
-//$ranking = change_htmlsp_array($data_ranking);
 
 include_once VIEW_PATH . 'index_view.php';
